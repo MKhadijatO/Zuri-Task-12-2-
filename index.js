1ebf99633878f8317aeed7a034688eff9ded33ba
Khadijat Muhammad <66877828+MKhadijatO@users.noreply.github.com>
2022-10-22 09:00:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const username = prompt ("What is your name?"); 
+
+
+console.log(`Welcome ${username}`)
+
+let points = 0;
+let levels = 1;
+let maxLevel =20;
+
+let guess = prompt ("Guess a number");
+
+let guessTheNumber = 
+Math.floor(Math.random() * 11);)
