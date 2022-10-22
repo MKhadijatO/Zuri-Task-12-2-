bdf23a6883e31d145423bced82a60e4eefb73c1a
Khadijat Muhammad <66877828+MKhadijatO@users.noreply.github.com>
2022-10-22 22:14:57 +0000

    Added break to stop the loop when game is over

--- index.js
+++ index.js
@@ -1,81 +1,34 @@
-// const { format } = require("path");
-
-const username = prompt ("What is your name?"); 
-
-
+const username = prompt ("Enter your name?"); 
 console.log(`Welcome ${username}`)
 
-let points = 1;
-// let j = 1;
-// let maxLevel = 5;
+
+
 
 let i = 2;
-let guess = prompt (`Guess a number between 1 and ${i++}`);
-console.log(+guess);
 
-let comGuess = Math.floor(Math.random(1)* i++);
 
-console.log(comGuess);
-
-if (+guess == comGuess) { 
-  console.log(`Congrats. Your point is ${points}`); i++;
+for (; i < 6; i++){
+  let guess = parseInt(prompt (`Guess a number between 1 and ${i}`));
+  console.log (`${username}, the number you guessed is ${guess}`)
   
-  // print($[guess]);
-//   do {
-//     let guess = prompt (`Guess a number between 1 and ${i+1}`);
-// console.log(+guess);
-
-// let comGuess = Math.floor(Math.random(1)* i+1; 1++);
-
-// console.log(comGuess);
-//   } while (1 <= 6);
+let comGuess = Math.floor(Math.random() * i) + 1;
+  console.log(`The correct number is ${comGuess}`)
+  
+  if (comGuess === guess){
+    console.log(`Congrats! Your point is  ${i-1}; 
+    Stage ${i}`)
   }
-else { 
-  console.log("Game Over")
+  else{
+    console.log("Game Over!")
+    break
+  }
+  
 }
 
 
-// do {
-//   if (+guess == comGuess) { 
-//   console.log(`Congrats. Your point is ${points}`);
-//   }
-// if else { 
-//   console.log("Game Over")
-// }; j++
-//  else { 
-//   console.log("Game Over")
-// }; j++ 
-// }
-// while (j < 5);
 
 
 
 
 
 
-
-
-
-
-
-
-// let reply = "Congratulation! "
-
-// for (let i = 0; i < 5; i++) {
-//   let comGuess = Math.floor(Math.random());
-//   console.log()
- //  let guess = prompt ("Guess a number");
-
- //  console.log(guess);
- //  let reply;
- //  if (guess < comGuess ) { reply = "Congrats";
- // } else {
- //   reply = "Game Over";
-// }
-  
-// }
-// let reply;
-// if (guess < comGuess ) { reply = "Congrats";
-//  } else {
-//    reply = "Game Over";
-// }
